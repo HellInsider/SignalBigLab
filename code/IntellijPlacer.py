2d5a5f2c48bfc6b11f272d8be1cce35e90410667
meso_test <45484407+HellInsider@users.noreply.github.com>
2022-04-22 19:50:38 +0300

    Refactored code after review.

--- code/IntellijPlacer.py
+++ code/IntellijPlacer.py
@@ -15,28 +15,26 @@ class IntellijPlacer:
     def __init__(self):
         self.img = None
         self.pics = []
-        self.binarized_objects = []
+        self.smallres_dir = '/small_res/'
 
-    def to_pol(self, x, y):
+    def to_polar_coordinated(self, x, y):
         theta = np.arctan2(y, x)
         rho = np.hypot(x, y)
         return theta, rho
 
-    def to_cart(self, theta, rho):
+    def to_cartesian_coordinates(self, theta, rho):
         x = rho * np.cos(theta)
         y = rho * np.sin(theta)
         return x, y
 
     def read_pics(self, path, pic_num):
-        print(os.getcwd())
+        #print("Reading pics...")
         self.get_pics(path, True)
-
-        self.binarized_objects = [self.binarize(t) for t in self.pics]
-
-        image = imageio.imread(path + "/small_res/" + str(pic_num) + ".jpg")
+        image = imageio.imread(path + self.smallres_dir + str(pic_num) + ".jpg")
         self.img = self.binarize(image)
 
-    def rotate(self, cnt, angle, img_size):         # поворот текстуры
+
+    def rotate(self, cnt, angle, img_size):
         M = cv.moments(cnt)
         cx = int(M['m10'] / M['m00'])
         cy = int(M['m01'] / M['m00'])
@@ -45,13 +43,13 @@ class IntellijPlacer:
 
         coordinates = cont_normal[:, 0, :]
         xs, ys = coordinates[:, 0], coordinates[:, 1]
-        thetas, rhos = self.to_pol(xs, ys)
+        thetas, rhos = self.to_polar_coordinated(xs, ys)
 
         thetas = np.rad2deg(thetas)
         thetas = (thetas + angle) % 360
         thetas = np.deg2rad(thetas)
 
-        xs, ys = self.to_cart(thetas, rhos)
+        xs, ys = self.to_cartesian_coordinates(thetas, rhos)
 
         cont_normal[:, 0, 0] = xs
         cont_normal[:, 0, 1] = ys
@@ -62,18 +60,17 @@ class IntellijPlacer:
         return cont_rotated
 
     def get_pics(self, path, is_small=False):                # загрузка изображений
-        if is_small and os.path.isdir(path + "/small_res"):
-            pics = [imageio.imread(path + "/small_res/" + fname) for fname in os.listdir(path + "/small_res") if
-                    fname.endswith(".jpg")]
+        if is_small and os.path.isdir(path + self.smallres_dir):
             return
+
         pics = [imageio.imread(path + "/" + fname) for fname in os.listdir(path) if fname.endswith(".jpg")]
 
         if is_small:
             pics = [resize(t, (512, 512 * t.shape[1] / t.shape[0])) for t in pics]              # сжатие изображений
-            pics = [t[0: t.shape[0] - 0, 0: t.shape[1] - 0] for t in pics]
-            os.mkdir(path + "/small_res")
+            pics = [t[0: t.shape[0], 0: t.shape[1]] for t in pics]
+            os.mkdir(path + self.smallres_dir)
             for i in range(len(pics)):
-                imageio.imwrite(path + "/small_res/" + str(i) + ".jpg", pics[i])
+                imageio.imwrite(path + self.smallres_dir + str(i) + ".jpg", pics[i])
 
     def cv2_image_from_imageio(self, img):
         return (img * 255).astype(np.uint8)
@@ -85,12 +82,12 @@ class IntellijPlacer:
         return self.cv2_image_from_imageio(new_edge_segmentation)
 
     def check_place(self, main_poly: np.ndarray):           # проверка расположений
-        err_cnt = 0
-        for el in main_poly.flat:
-            if el > 255:
-                err_cnt += 1
-                if err_cnt > main_poly.shape[0] * main_poly.shape[1] / 5000:
-                    return False
+        max_err_rate = 0.001
+
+        err_cnt = np.count_nonzero(main_poly > 255)
+        if err_cnt > main_poly.shape[0] * main_poly.shape[1] * max_err_rate:
+            return False
+
         return True
 
     def check_shape(self, destination: np.ndarray, objects):  # Проверка размеров объектов
@@ -101,28 +98,33 @@ class IntellijPlacer:
         return True
 
     def fit_iteration(self, destination: np.ndarray, objects, idx):     # итерация расположений одного объекта
-        print('  ' * idx, idx)
+        #print('  ' * idx, "Object num: ", idx)
         object = objects[idx]
         rows, cols = object.shape
         angle = 0
         step = 3
         percent = 0.05
 
-        while angle <= 360:                                                              # поворот
-            print('  ' * idx, 'angle =', angle)
+        while angle in range(0, 360, step):                                                              # поворот
+            #print('  ' * idx, 'angle =', angle)
             for half_trans_y in range(destination.shape[0] // 2):
                 for half_trans_x in range(destination.shape[1] // 2):                       # перемещение по Х и У
 
                     trans_y = 2 * half_trans_y
                     trans_x = 2 * half_trans_x
-                    M = cv.getRotationMatrix2D(((cols - 1) / 2.0, (rows - 1) / 2.0), angle, 1)
-                    rotated_object = cv.warpAffine(object, M, (cols, rows))
+
+                    Matrix = cv.getRotationMatrix2D(((cols - 1) / 2.0, (rows - 1) / 2.0), angle, 1)
+                    rotated_object = cv.warpAffine(object, Matrix, (cols, rows))
+
                     copy = np.ones(shape=(
-                    destination.shape[0] + 2 * object.shape[0], destination.shape[1] + 2 * object.shape[1])) * 255
+                        destination.shape[0] + 2 * object.shape[0], destination.shape[1] + 2 * object.shape[1])) * 255
+
                     copy[object.shape[0]:object.shape[0] + destination.shape[0],
-                    object.shape[1]:object.shape[1] + destination.shape[1]] = destination
+                        object.shape[1]:object.shape[1] + destination.shape[1]] = destination
+
                     copy[trans_y + object.shape[0]:trans_y + 2 * object.shape[0],
-                    trans_x + rotated_object.shape[1]:trans_x + 2 * rotated_object.shape[1]] += rotated_object
+                        trans_x + rotated_object.shape[1]:trans_x + 2 * rotated_object.shape[1]] += rotated_object
+
                     if self.check_place(copy):
                         if idx == len(objects) - 1:
                             return copy
@@ -130,38 +132,34 @@ class IntellijPlacer:
                             rez = self.fit_iteration(copy, objects, idx + 1)
                             if rez is not None:
                                 return rez
+
                     half_trans_x += int(percent * destination.shape[1] // 2)
                 half_trans_y += int(percent * destination.shape[0] // 2)
-            angle += step
         return None
 
-    def try_fit(self):                                                          # проверка на возможность располажение всех объектов
-        contours, hierarchy = cv.findContours(self.img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-        counters_modified = []
-        res_counters = np.zeros((self.img.shape[0], self.img.shape[1], 3), dtype=np.uint8)
+    def check_image(self):
+        min_area_size = 50
+        res = False                         # проверка на возможность располажения всех объектов
+        contours, hierarchy = cv.findContours(self.img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        contours_modified = []
         rects = []
         max_area_idx = 0
         max_area = cv.contourArea(contours[0])
         for i in range(len(contours)):
             area = cv.contourArea(contours[i])
-            if hierarchy[0][i][3] != -1 or area <= 200:
+            if hierarchy[0][i][3] != -1 or area <= min_area_size:
                 continue
             if area > max_area:
                 max_area, max_area_idx = area, len(rects)
 
-            counters_modified.append(contours[i])
+            contours_modified.append(contours[i])
             rects.append(cv.minAreaRect(contours[i]))
-            color = (rng.randint(0, 256), rng.randint(0, 256), rng.randint(0, 256))
-            cv.drawContours(res_counters, contours, i, color, 2, cv.LINE_8, hierarchy, 0)
-            box = cv.boxPoints(rects[-1])
-            box = np.int0(box)
-            cv.drawContours(res_counters, [box], 0, (0, 0, 255), 2)
 
         masks = []
-        for rec in rects:
-            masks.append(np.zeros((int(rec[1][1]), int(rec[1][0]))).astype(np.uint8))
-            rotated_cnt = counters_modified[len(masks) - 1]
-            rotated_cnt = self.rotate(rotated_cnt, -rec[2], rec[1])
+        for rect in rects:
+            masks.append(np.zeros((int(rect[1][1]), int(rect[1][0]))).astype(np.uint8))
+            rotated_cnt = contours_modified[len(masks) - 1]
+            rotated_cnt = self.rotate(rotated_cnt, -rect[2], rect[1])
             cv.fillPoly(masks[-1], pts=[rotated_cnt], color=(255, 255, 255))
 
         polygon = masks.pop(max_area_idx)
@@ -175,8 +173,9 @@ class IntellijPlacer:
             main_poly = self.fit_iteration(main_poly, masks, 0)
 
         if main_poly is None:
-            print("No")
+            res = False
         else:
-            print("Yes")
             cv.imshow("main_poly", main_poly)
+            res = True
         cv.waitKey()
+        return res
--- code/main.py
+++ code/main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     placer = IntellijPlacer()
     print(os.getcwd())
     placer.read_pics('..\dataset', 0)   # второй параметр - номер картинки в дирактории. Начинается с 0.
-    placer.try_fit()
+    print(placer.check_image())
 
 
 
